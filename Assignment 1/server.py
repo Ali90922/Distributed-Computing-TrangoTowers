3311d1502761d387ad46cae81b85cb559451f4d9
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2024-09-11 20:00:26 -0500

    Version update

--- Assignment 1/server.py
+++ Assignment 1/server.py
@@ -1,4 +1,29 @@
+# ----------------------------------------------------------------------------------------------
+# Name: Ali Nawaz
+# Student number: 7951458
+# Course: COMP 3010, Distributed Computing
+# Instructor: Saulo 
+# Assignment: Assignment 1, chat_server.py
+# 
+# Remarks: Server for our client-server application
+#
+#-------------------------------------------------------------------------------------------------
+
+
 import socket
 import threading
 
-server = socket.socket(socket.AF) 
+
+
+host = '127.0.0.1'    # Local host Ip address
+port = 55456
+
+
+
+# The first parameter means we are using internet socket and  second indicates we are using TCP and not UDP.
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+
+
+server.bind((host,port))     # Binding the server to our host
+server.listen()           # Server starts listening to incoming methods
+
