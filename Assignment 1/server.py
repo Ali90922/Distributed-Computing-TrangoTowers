3dc5cd5bf5cf0da1cc0500223a8d29f8c4fae1f6
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2024-09-11 21:51:37 -0500

    ChatServer done, starting work on client

--- Assignment 1/server.py
+++ Assignment 1/server.py
@@ -27,3 +27,56 @@ server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((host,port))     # Binding the server to our host
 server.listen()           # Server starts listening to incoming methods
 
+clients = []
+Nicknames = []
+
+
+
+# Broadcast function
+def Broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+
+# Handle the client 
+        def handle(client):
+            while(True):
+                try:
+                    message = client.recv(1024)
+                    Broadcast(message)
+                except:
+                    index = clients.index(client)
+                    clients.remove(client)
+                    client.close()
+                    Nickname = Nicknames[index]
+                    Broadcast(f'{Nickname} has left the chat !!'.encode('ascii'))
+                    Nicknames.remove(Nickname)
+                    break
+
+
+
+def recieve():
+    while True:
+        client,address = server.accept()
+        print(f"Connected with {str(address)}")
+        client.send("NICK".encode('ascii'))
+        Nickname = client.recv(1024).decode('ascii')
+        Nicknames.append(Nickname)
+        clients.append(client)
+
+        print(f'Nickname of the client is {Nickname} !')
+        Broadcast(f'{Nickname} joined the chat lads !'.encode(ascii))
+        client.send('You are connected to the server bud!'.encode(ascii))
+
+
+        thread = threading.Thread(target=handle,args=(client,))
+        thread.start()
+print("Server is listening ......")
+recieve()          
+
+
+
+
+
+
