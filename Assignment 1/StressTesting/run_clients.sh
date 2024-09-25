04497ed89765ee5424ef897e646826b47f273c63
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2024-09-25 12:16:54 -0500

    Bash Client script

---
+++ Assignment 1/StressTesting/run_clients.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Number of clients passed as an argument
+num_clients=$1
+
+# Path to your Python client file
+client_script="SpamClient.py"
+
+# Function to run a single client
+run_client() {
+    python3 "$client_script" &
+}
+
+# Launch the specified number of clients
+echo "Running with $num_clients clients..."
+
+for ((i = 1; i <= num_clients; i++)); do
+    run_client
+done
+
+# Let the clients run for 5 minutes (300 seconds) before killing them
+sleep 300
+
+# Kill all clients after test period
+echo "Stopping clients..."
+pkill -f "$client_script"
+
+echo "Test complete!"
+
+
+
+
+
+# Command to make to script executeble on my AWS machine : chmod +x run_clients.sh
+# How to run the script with the commandline arguments : ./run_clients.sh 10
